9b8aaae770035a45057ae442dcc4e33767e37026
suniljadhav1812 <62737248+suniljadhav1812@users.noreply.github.com>
2025-07-04 10:16:07 +0530

    Update 2_Stability_Test.py

--- pages/2_Stability_Test.py
+++ pages/2_Stability_Test.py
@@ -159,7 +159,8 @@ if uploaded_file:
     filtered_df = final_df[~final_df['Cert. Val.'].astype(str).str.contains("-", na=False)]
     element_summary = filtered_df.groupby("Elements").apply(summarize_stability).reset_index()
     element_summary.columns = ["Elements", "Stability_Result"]
-
+    element_summary["Elements"] = element_summary["Elements"].str.title()
+    final_df["Elements"] = final_df["Elements"].str.capitalize()
     st.subheader("📋 Stability Result Summary")
     st.dataframe(element_summary)
 
